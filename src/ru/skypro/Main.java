6c5e62292c203662c0d8666a7ad4c96900e0d458
Саша <saniamit@mail.ru>
2021-12-14 01:01:40 +0300

    ДЗ по STRING (Работа над ошибками)

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -10,19 +10,22 @@ public class Main {
         String firstName = "Ivan";
         String middleName = "Ivanovich";
         String lastname = "Ivanov";
-        String fullName = (firstName + " " + middleName + " " + lastname);
+        String fullName = firstName + " " + middleName + " " + lastname;
         System.out.println(" * Задача 1 ");
         System.out.println(" ФИО сотрудника — " + fullName);
+
         System.out.println(" * Задача 2 ");
-        System.out.println(fullName.toUpperCase(Locale.ROOT));
+        System.out.println(fullName.toUpperCase());
+
         System.out.println(" * Задача 3 ");
-        String result = fullName.replace(" ", ";");
+        String result = fullName.replace( " ", ";");
         System.out.println(" Данные ФИО сотрудника для административного отдела - " + result);
 
-        String fullName2 = " Иванов Семён Семёнович";
-        String chooseName = fullName2.replace(" ё", " е");
+
+        String fullName2 = "Иванов Семён Семёнович";
+        String chooseName = fullName2.replace('ё', 'е');
         System.out.println(" * Задача 4 ");
-        System.out.println(" Данные ФИО сотрудника - " + chooseName);
+        System.out.println("Данные ФИО сотрудника - " + chooseName);
 }
 }
 
